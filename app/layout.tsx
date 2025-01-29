b4c38270b585f9bf8f17a31e84e4e3b2196c00e5
Marckenley Dorsainvil <marckenlygbeau@gmail.com>
2025-01-29 09:04:59 +0400

    adding SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,37 @@ import "./globals.css";
 import { ThemeProvider } from "next-themes";
 
 const inter = Inter({ subsets: ["latin"] });
+const structuredData = {
+  "@context": "https://schema.org",
+  "@type": "Person",
+  "name": "Marckenley Dorsainvil",
+  "jobTitle": "Full-Stack Developer",
+  "url": "https://www.marcshowcase.com",
+  "sameAs": [
+    "https://github.com/Marckenleydev",
+    "https://www.linkedin.com/in/marckenley-dorsainvil",
+    "https://twitter.com/yourprofile"
+  ],
+  "worksFor": {
+    "@type": "Organization",
+    "name": "Comnerds Web Technology and Design Tic. Ltd. Şti."
+  },
+  "alumniOf": {
+    "@type": "CollegeOrUniversity",
+    "name": "Istanbul Aydin University"
+  },
+  "description": "Experienced Full-Stack Developer skilled in Java (Spring Boot), Node.js (TypeScript, JavaScript), and React, specializing in building scalable and robust web applications.",
+  "knowsAbout": [
+    "Java", "Spring Boot", "MongoDB", "React", "AWS", "Microservices","NodeJS", "JavaScript", "TypeScript", 
+  ]
+};
 
 export const metadata: Metadata = {
   title: "Marckenley's Portfolio",
-  description: "Spring boot & Next portfolio",
+  description: "Spring Boot & Next.js Portfolio",
+  keywords: "Full-Stack Developer, Java, Spring Boot, React, MongoDB, AWS,NodeJS, JavaScript,Typescript",
+  robots: "index, follow",
+  
 };
 
 export default function RootLayout({
---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    siteUrl: 'https://www.marcshowcase.com/',
+    generateRobotsTxt: true, // Generate robots.txt file
+  };
+  
